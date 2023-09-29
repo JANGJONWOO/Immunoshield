858e07aeb3f337a165828478d757d4f2c20096c4
Shantanu <97342128+JANGJONWOO@users.noreply.github.com>
2023-09-29 20:34:04 +0530

    This is our first group project

---
+++ register_js.js
@@ -0,0 +1,9 @@
+var x = document.getElementById("doctor");
+var y = document.getElementById("patient");
+var z = document.getElementById("btn");
+
+function patient(){
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
